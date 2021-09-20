ca162017574467865192441f665c4abb3a3c1d26
Sunday Morenikeji <65197762+areel007@users.noreply.github.com>
2021-09-20 22:07:51 +0100

    employment screen

--- Documents/GitHub/vangold/src/component/core-ui/steps/employment.js
+++ Documents/GitHub/vangold/src/component/core-ui/steps/employment.js
@@ -11,105 +11,112 @@ import 'react-pure-modal/dist/react-pure-modal.min.css';
 
 const Employment = (props) => {
     const [modal, setModal] = useState(false);
-    const [educationHistory, setEducationHistory] = useState([])
-    const [schoolInput, setSchoolInput] = useState("")
-    const [schoolDegree, setSchoolDegree] = useState("")
-    const [course, setCourse] = useState("")
-    // const [dataToDisplay] = useState(years)
-    const [showDropdownMonth, setShowDropdownMonth] = useState(false)
-    const [showDropdownMonthTwo, setShowDropdownMonthTwo] = useState(false)
-    const [showDropdownYears, setShowDropdownYears] = useState(false)
-    const [showDropdownYearsTwo, setShowDropdownYearsTwo] = useState(false)
-    const [selectDropdownHeaderMonth, setSelectDropdownHeaderMonth] = useState("Month")
-    const [selectDropdownHeaderMonthTwo, setSelectDropdownHeaderMonthTwo] = useState("Month")
-    const [selectDropdownHeaderYears, setSelectDropdownHeaderYears] = useState("Year")
-    const [selectDropdownHeaderYearsTwo, setSelectDropdownHeaderYearsTwo] = useState("Year")
+    const [employmentHistory, setEmploymentHistory] = useState([])
+    const [company, setCompany] = useState("")
+    const [position, setPosition] = useState("")
+    const [showDropdownMonthFrom, setShowDropdownMonthFrom] = useState(false)
+    const [showDropdownMonthTo, setShowDropdownMonthTo] = useState(false)
+    const [showDropdownYearsFrom, setShowDropdownYearsFrom] = useState(false)
+    const [showDropdownYearsTo, setShowDropdownYearsTo] = useState(false)
+    const [selectDropdownHeaderMonthFrom, setSelectDropdownHeaderMonthFrom] = useState("Month")
+    const [selectDropdownHeaderMonthTo, setSelectDropdownHeaderMonthTo] = useState("Month")
+    const [selectDropdownHeaderYearsFrom, setSelectDropdownHeaderYearsFrom] = useState("Year")
+    const [selectDropdownHeaderYearsTo, setSelectDropdownHeaderYearsTo] = useState("Year")
     // const [from, setFrom] = useState("")
     // const [to, setTo] = useState("")
 
     //Actions - Methods
 
-    const upDateDropdownHeaderMonth = (event) => {
-        setSelectDropdownHeaderMonth(event.target.innerText)
-        setShowDropdownMonth(false)
+    const upDateDropdownHeaderMonthFrom = (event) => {
+        setSelectDropdownHeaderMonthFrom(event.target.innerText)
+        setShowDropdownMonthFrom(false)
     }
-    const upDateDropdownHeaderMonthTwo = (event) => {
-        setSelectDropdownHeaderMonthTwo(event.target.innerText)
-        setShowDropdownMonthTwo(false)
-    }
-
-    const upDateDropdownHeaderYears = (event) => {
-        setSelectDropdownHeaderYears(event.target.innerText)
-        setShowDropdownYears(false)
-    }
-    const upDateDropdownHeaderYearsTwo = (event) => {
-        setSelectDropdownHeaderYearsTwo(event.target.innerText)
-        setShowDropdownYearsTwo(false)
+    const upDateDropdownHeaderMonthTo = (event) => {
+        setSelectDropdownHeaderMonthTo(event.target.innerText)
+        setShowDropdownMonthTo(false)
     }
 
-    const toggleDropdownMonth = () => {
-        if (!showDropdownMonth) {
-            setShowDropdownMonth(true)
-        } else {
-            setShowDropdownMonth(false)
-        }
+    const upDateDropdownHeaderYearsFrom = (event) => {
+        setSelectDropdownHeaderYearsFrom(event.target.innerText)
+        setShowDropdownYearsFrom(false)
     }
-    const toggleDropdownMonthTwo = () => {
-        if (!showDropdownMonthTwo) {
-            setShowDropdownMonthTwo(true)
-        } else {
-            setShowDropdownMonthTwo(false)
-        }
+    const upDateDropdownHeaderYearsTo = (event) => {
+        setSelectDropdownHeaderYearsTo(event.target.innerText)
+        setShowDropdownYearsTo(false)
     }
 
-    const toggleDropdownYears = () => {
-        if (!showDropdownYears) {
-            setShowDropdownYears(true)
+    const toggleDropdownMonthFrom = () => {
+        if (!showDropdownMonthFrom) {
+            setShowDropdownMonthFrom(true)
         } else {
-            setShowDropdownYears(false)
+            setShowDropdownMonthFrom(false)
         }
     }
-    const toggleDropdownYearsTwo = () => {
-        if (!showDropdownYears) {
-            setShowDropdownYearsTwo(true)
+    const toggleDropdownMonthTo = () => {
+        if (!showDropdownMonthTo) {
+            setShowDropdownMonthTo(true)
         } else {
-            setShowDropdownYearsTwo(false)
+            setShowDropdownMonthTo(false)
         }
     }
 
-    const onFormSubmit = () => {
-        setEducationHistory([
-            ...educationHistory,
+    const toggleDropdownYearsFrom = () => {
+        if (!showDropdownYearsFrom) {
+            setShowDropdownYearsFrom(true)
+        } else {
+            setShowDropdownYearsFrom(false)
+        }
+    }
+    const toggleDropdownYearsTo = () => {
+        if (!showDropdownYearsTo) {
+            setShowDropdownYearsTo(true)
+        } else {
+            setShowDropdownYearsTo(false)
+        }
+    }
+
+    const onCompany = (event) => {
+        setCompany(event.target.value)
+    }
+
+    const onPosition = (event) => {
+        setPosition(event.target.value)
+    }
+
+    const onFormSubmit = (e) => {
+        setEmploymentHistory([
+            ...employmentHistory,
             {
-                schoolName: schoolInput,
-                degree: schoolDegree,
-                course: course,
+                company: company,
+                position: position,
                 id: uuidv4(),
-                from: selectDropdownHeaderMonth,
-                to: selectDropdownHeaderYears
+                monthFrom: selectDropdownHeaderMonthFrom,
+                monthTo: selectDropdownHeaderMonthTo,
+                yearsFrom: selectDropdownHeaderYearsFrom,
+                yearsTo: selectDropdownHeaderYearsTo
             }
         ])
-        setSchoolInput("")
-        setSchoolDegree("")
+        setPosition("")
+        setCompany("")
+        setSelectDropdownHeaderMonthFrom("Month")
+        setSelectDropdownHeaderMonthTo("Month")
+        setSelectDropdownHeaderYearsFrom("Year")
+        setSelectDropdownHeaderYearsTo("Year")
+
     }
 
-    const editHistory = (history) => {
-        setModal(true)
-        setSchoolInput(history.schoolName)
-        setSchoolDegree(history.degree)
-        if (schoolInput !== history.schoolName || schoolDegree !== history.degree) {
-            deleteHistory(history)
-        }
+    const editEmployment = (history) => {
+        alert('hhdjdj')
     }
 
-    const deleteHistory = ({id}) => {
-        setEducationHistory(educationHistory.filter((education) => education.id !== id))
+    const deleteEmployment = ({id}) => {
+        alert('hfhhfh')
     }
 
     let textInput = React.createRef();  // React use ref to get input value
 
   let onOnclickHandler = (e) => {
-    console.log(textInput.current.value); 
+    console.log(textInput.current.value);
   };
     return (
         <div className="step">
@@ -117,13 +124,12 @@ const Employment = (props) => {
             <span className="step-subtitle">Add your work experience and boost your credibility in the process</span>
             <div className="education-history__education">
                 {
-                    educationHistory.map((_educationHistory, index) => {
-                        return <div className="education-history" key={index}>
-                            <div className="school-name-and-degree">
-                                <p className="school-name">{_educationHistory.schoolName}</p>
-                                <p className="degree">
-                                    {_educationHistory.degree} - {_educationHistory.course} <br/>
-                                    {_educationHistory.from} - {_educationHistory.to}
+                    employmentHistory.map((_employment, index) => {
+                        return <div className="employment-history" key={index}>
+                            <div className="company-and-position">
+                                <p className="school-name">{_employment.position} | {_employment.company} </p>
+                                <p className="date-from-to">
+                                    {_employment.monthFrom} {_employment.yearsFrom} - {_employment.monthTo.toLowerCase()==='present'?'Present':_employment.monthTo} {_employment.monthTo.toLowerCase()==='present'?null:_employment.yearsTo}
                                 </p>
                             </div>
                             <div className="edit-and-delete-icon">
@@ -133,7 +139,7 @@ const Employment = (props) => {
                                         height="19"
                                         viewBox="0 0 19 19"
                                         fill="none"
-                                        onClick={() => editHistory(_educationHistory)}
+                                        onClick={null}
                                     >
                                         <path d="M0 15.2496V18.9996H3.75L14.81 7.93957L11.06 4.18957L0 15.2496ZM17.71 5.03957C18.1 4.64957 18.1 4.01957 17.71 3.62957L15.37 1.28957C14.98 0.89957 14.35 0.89957 13.96 1.28957L12.13 3.11957L15.88 6.86957L17.71 5.03957Z" fill="#808080"/>
                                     </svg>
@@ -144,7 +150,7 @@ const Employment = (props) => {
                                         height="18"
                                         viewBox="0 0 14 18"
                                         fill="none"
-                                        onClick={() => deleteHistory(_educationHistory)}
+                                        onClick={null}
                                     >
                                         <path d="M1 16C1 17.1 1.9 18 3 18H11C12.1 18 13 17.1 13 16V4H1V16ZM14 1H10.5L9.5 0H4.5L3.5 1H0V3H14V1Z" fill="#808080"/>
                                     </svg>
@@ -171,31 +177,31 @@ const Employment = (props) => {
                 }}
                 >
                 <div className="edu__form">
-                    <form action="">
+                    <form>
                         <div className="form-input">
                             <label htmlFor="Company">Company</label>
-                            <input type="text" />
+                            <input type="text" value={company} onChange={onCompany} />
                         </div>
                         <div className="form-input">
                             <label htmlFor="Position">Position</label>
-                            <input type="text" />
+                            <input type="text" value={position} onChange={onPosition} />
                         </div>
                         <div className="datePickerBox">
                             <label htmlFor="Duration of Employment">Duration of Employment</label>
                             <div className="inn__datePickerBox">
                             <SelectDropdown
                                     dataToDisplay={months}
-                                    toggleDropdown={toggleDropdownMonth}
-                                    showDropdown={showDropdownMonth}
-                                    selectDropdownHeader={selectDropdownHeaderMonth}
-                                    upDateDropdownHeader={upDateDropdownHeaderMonth}
+                                    toggleDropdown={toggleDropdownMonthFrom}
+                                    showDropdown={showDropdownMonthFrom}
+                                    selectDropdownHeader={selectDropdownHeaderMonthFrom}
+                                    upDateDropdownHeader={upDateDropdownHeaderMonthFrom}
                                 />
                                 <SelectDropdown
                                     dataToDisplay={years}
-                                    toggleDropdown={toggleDropdownYears}
-                                    showDropdown={showDropdownYears}
-                                    selectDropdownHeader={selectDropdownHeaderYears}
-                                    upDateDropdownHeader={upDateDropdownHeaderYears}
+                                    toggleDropdown={toggleDropdownYearsFrom}
+                                    showDropdown={showDropdownYearsFrom}
+                                    selectDropdownHeader={selectDropdownHeaderYearsFrom}
+                                    upDateDropdownHeader={upDateDropdownHeaderYearsFrom}
                                 />
                             </div>
                         </div>
@@ -204,17 +210,17 @@ const Employment = (props) => {
                             <div className="inn__datePickerBox">
                             <SelectDropdown
                                     dataToDisplay={months}
-                                    toggleDropdown={toggleDropdownMonthTwo}
-                                    showDropdown={showDropdownMonthTwo}
-                                    selectDropdownHeader={selectDropdownHeaderMonthTwo}
-                                    upDateDropdownHeader={upDateDropdownHeaderMonthTwo}
+                                    toggleDropdown={toggleDropdownMonthTo}
+                                    showDropdown={showDropdownMonthTo}
+                                    selectDropdownHeader={selectDropdownHeaderMonthTo}
+                                    upDateDropdownHeader={upDateDropdownHeaderMonthTo}
                                 />
                                 <SelectDropdown
                                     dataToDisplay={years}
-                                    toggleDropdown={toggleDropdownYearsTwo}
-                                    showDropdown={showDropdownYearsTwo}
-                                    selectDropdownHeader={selectDropdownHeaderYearsTwo}
-                                    upDateDropdownHeader={upDateDropdownHeaderYearsTwo}
+                                    toggleDropdown={toggleDropdownYearsTo}
+                                    showDropdown={showDropdownYearsTo}
+                                    selectDropdownHeader={selectDropdownHeaderYearsTo}
+                                    upDateDropdownHeader={upDateDropdownHeaderYearsTo}
                                 />
                             </div>
                         </div>
@@ -230,7 +236,7 @@ const Employment = (props) => {
                             <button className="button1" onClick={() => setModal(false)} color="primary">
                                 Cancel
                             </button>
-                            <button className="button2" onClick={ () => { onOnclickHandler(); onFormSubmit(); setModal(false) } } color="primary">
+                            <button className="button2" onClick={ () => { onFormSubmit(); setModal(false) } } color="primary">
                                 Save
                             </button>
                         </div>
