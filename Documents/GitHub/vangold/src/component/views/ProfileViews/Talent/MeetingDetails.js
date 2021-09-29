9c79f5500fc987d912099c14b699b201dec92805
Ahmer Altaf <ahmeraltaf69@gmail.com>
2021-09-29 15:52:18 +0500

    some mobile screens and two brand interfaces are done

---
+++ Documents/GitHub/vangold/src/component/views/ProfileViews/Talent/MeetingDetails.js
@@ -0,0 +1,93 @@
+import { Card, Col, Container, Row } from "react-bootstrap"
+import SideMenu from "../../../core-ui/SideMenu/SideMenu"
+import clock from "../../../../assets/images/profile/clock.png";
+import add from "../../../../assets/images/profile/add.png";
+import user1 from "../../../../assets/images/profile/user1.png";
+import pencil1 from "../../../../assets/images/profile/pencil1.png";
+import dustbin from "../../../../assets/images/profile/dustbin.png";
+import avatar4 from "../../../../assets/images/profile/avatar4.png";
+import avatar5 from "../../../../assets/images/profile/avatar5.png";
+import avatar6 from "../../../../assets/images/profile/avatar6.png";
+import './Talent.css'
+
+const persons = [
+    {
+        img: avatar4,
+        name: "Vivian Miles"
+    },
+    {
+        img: avatar5,
+        name: "Jackie Robinsin"
+    },
+    {
+        img: avatar6,
+        name: "Clark Griffen"
+    },
+]
+
+const MeetingDetails = () => {
+
+    return (
+        <section id="talent" className="my-3 py-3">
+            <Container fluid>
+                <Row>
+                    <Col xs={10} lg={2} className="mx-auto mt-5 d-none d-lg-block">
+                        <SideMenu />
+                    </Col>
+                    <Col xs={12} md={10} lg={10} className="mx-auto bg-color">
+                        <h1 className='meeting-head'>Vangold Daily Standup Meeting</h1>
+                        <p className='meeting-text'><img src={clock} alt="clock" className='mb-1' /> 02:00 - 03:03 | starts in 02 hours</p>
+
+                        <div className='d-lg-flex' style={{ marginTop: "4rem" }}>
+                            <div className='mb-4'>
+                                <button className='meeting-btn me-4'>Start</button>
+                                <button className='meeting-btn1 me-4'>Copy Invitation</button>
+                            </div>
+                            <div>
+                                <button className='meeting-btn1 me-4'><img src={pencil1} alt="pencil" /></button>
+                                <button className='meeting-btn1'><img src={dustbin} alt="dustbin" /></button>
+                            </div>
+                        </div>
+                        <Row style={{ marginTop: "4rem" }}>
+                            <Col xs={10} md={6} >
+                                <p className='meeting-text'>The purpose of this meeting is to discuss the progress of the job done so far and to set a timeline for the launch of the feature.</p>
+                            </Col>
+                        </Row>
+
+                        <div className='meeting-id'>
+                            <h1 className='meet-id-head'>Meeting ID</h1>
+                            <p className='meet-id-code'>969 042 3871</p>
+                        </div>
+                        <div className='d-flex' style={{ marginTop: "4rem" }}>
+                            <div className='add-meet text-center'>
+                                <img src={user1} alt="user" className='img-fluid mt-4' />
+                            </div>
+                            <p className='meet-part'>Participants:</p>
+                        </div>
+
+                        <Row className='mt-5'>
+                            {persons.map((person) => (
+                                <Col xs={12} md={3} lg={2} className='mb-4'>
+                                    <div className='participant-bg p-5 text-center'>
+                                        <img src={person.img} alt="avatar" className='img-fluid' />
+                                        <p className='participant-text'>{person.name}</p>
+                                    </div>
+                                </Col>
+                            ))}
+                            <Col xs={12} md={3} lg={2} className='mb-4'>
+                                <div className='invite-bg p-5 text-center'>
+                                    <div className="icon-bg mx-auto">
+                                        <img src={add} alt="add" className='img-fluid img-bg' />
+                                    </div>
+                                    <p className='participant-text' style={{ color: "#fff" }}>Invite Member</p>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+}
+
+export default MeetingDetails;
